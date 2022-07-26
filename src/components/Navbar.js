0884982b03d0cdd832e22470ffacea284a946960
Hari-str <haristr5235@gmail.com>
2022-07-26 15:28:36 +0530

    sidedrawer create

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { MdOutlineMenu, MdOutlineClose } from "react-icons/md";
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const [icon, setIcon] = useState(false);
+
+    const handleClick = () => {
+        setIcon(!icon)
+    }
+
+    const closeSideDrawer = () => {
+        setIcon(false)
+    }
     return (
         <>
             <nav>
                 <div className="navbar">
                     <div className="navbar__logo">
-                        <Link to="/">Logo</Link>
+                        <Link to="/" onClick={closeSideDrawer}>Logo</Link>
                     </div>
-                    <div className="navbar__links">
-                        <ul>
+                    <div className="navbar__item">
+                        <ul className={
+                            icon ? 'navbar__links active' : 'navbar__links'
+                        }>
                             <li>
-                                <Link to='/'>Home</Link>
+                                <Link to='/' onClick={closeSideDrawer}>Home</Link>
                             </li>
                             <li>
-                                <Link to='/products'>Products</Link>
+                                <Link to='/products' onClick={closeSideDrawer}>Products</Link>
                             </li>
                             <li>
-                                <Link to='/about'>About</Link>
+                                <Link to='/about' onClick={closeSideDrawer}>About</Link>
                             </li>
                             <li>
-                                <Link to='/contact'>Contact</Link>
+                                <Link to='/contact' onClick={closeSideDrawer}>Contact</Link>
                             </li>
-                            <li>
-                                <Link to='/signup'>SignUp</Link>
+                            <li className='signup-btn' >
+                                <Link to='/signup' onClick={closeSideDrawer}>SignUp</Link>
                             </li>
                         </ul>
+                    </div>
+                    <div className='nav-toggle' onClick={handleClick}>
+                        {
+                            icon ? <MdOutlineClose /> : <MdOutlineMenu />
+                        }
                     </div>
                 </div>
             </nav>
